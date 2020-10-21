7aed9d3cdd5f645e590b909cb04312ba46366896
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-10-21 10:06:14 -0500

    added a list to contain blog posts

--- src/main/java/com/tts/blog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/blog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.tts.blog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +15,24 @@ public class BlogPostController {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
+    private List<BlogPost> posts = new ArrayList<>();
+
     //localhost:8080 (/) automatically fills in
 
     @GetMapping(value = "/")
-    public String index(BlogPost blogPost) {
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
         return "blogpost/index";
     }
 
     @PostMapping(value = "/")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
 
+
         blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+
+        posts.add(blogPost);
+
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
