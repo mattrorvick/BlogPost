acea2b538da63564adecc79c63f10883b5594773
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-10-21 09:59:10 -0500

    submitting the inputs from user

--- src/main/java/com/tts/blog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/blog/BlogPost/BlogPostController.java
@@ -1,17 +1,34 @@
 package com.tts.blog.BlogPost;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogPostController {
 
+    @Autowired
+    private BlogPostRepository blogPostRepository;
 
     //localhost:8080 (/) automatically fills in
 
     @GetMapping(value = "/")
     public String index(BlogPost blogPost) {
         return "blogpost/index";
+    }
+
+    @PostMapping(value = "/")
+    public String addNewBlogPost(BlogPost blogPost, Model model) {
+
+        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        model.addAttribute("title", blogPost.getTitle());
+        model.addAttribute("author", blogPost.getAuthor());
+        model.addAttribute("blogEntry", blogPost.getBlogEntry());
+
+
+        return "blogpost/result";
     }
 
 
