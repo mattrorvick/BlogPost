91f1849f257af5acbd93cf9a206d5182ecc5300e
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-10-21 09:13:03 -0500

    created blog post repository

---
+++ src/main/java/com/tts/blog/BlogPost/BlogPostRepository.java
@@ -0,0 +1,10 @@
+package com.tts.blog.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+    
+    
+
+
+}
